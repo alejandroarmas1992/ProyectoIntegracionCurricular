d5bebf8aef0c8bcb86b833beb34c8604837e39d9
alejandroarmas1992 <l_jandro@hotmail.com>
2023-03-04 19:00:50 -0500

    scss header

--- views/auth/olvide.php
+++ views/auth/olvide.php
@@ -1,6 +1,6 @@
 <main class="auth">
     <h2 class="auth_heading"><?php echo $titulo; ?></h2>
-    <p class="aut_text">Inicio sesión en DebWebCamp</p>
+    <p class="aut_text">Recupera tu acceso a DevWebCamp</p>
 
 
     <form class="formulario">
@@ -14,22 +14,13 @@
                 name="email"
             >
         </div>
-        <div class="formulario__campo">
-            <label for="password" class="formulario__label">Password</label>
-            <input
-                type="password"
-                class="formulario_input"
-                placeholder="Tu Password"
-                id="password"
-                name="password"
-            >
-        </div>
-        <input type="submit" class="formulario__submit" value="Iniciar Sesión" />
+       
+        <input type="submit" class="formulario__submit" value="Enviar Instrucciones" />
     </form>
 
     <div class="acciones">
+        <a href="/login" class="acciones__enlace">¿Ya tienes cuenta? Iniciar Sesión</a>
         <a href="/registro" class="acciones__enlace">¿Aún no tienes cuenta? Obtener una</a>
-        <a href="/olvide" class="acciones__enlace">¿Olvidaste tu password?</a>
     </div>
 
 </main>
